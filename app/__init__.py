eab792c6674ac385777147797029752fbf527597
Christopher <crisson.christopher@gmail.com>
2020-06-22 20:39:28 -0400

    indentation error

--- app/__init__.py
+++ app/__init__.py
@@ -54,13 +54,13 @@ def create_app(config_class=Config):
       stream_handler.setLevel(loggin.INFO)
       app.logger.addHandler(stream_handler)
     else:
-    if not os.path.exists('logs'):
-      os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/library.log', maxBytes=10240, backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+      if not os.path.exists('logs'):
+        os.mkdir('logs')
+      file_handler = RotatingFileHandler('logs/library.log', maxBytes=10240, backupCount=10)
+      file_handler.setFormatter(logging.Formatter(
+          '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+      file_handler.setLevel(logging.INFO)
+      app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Library startup')
