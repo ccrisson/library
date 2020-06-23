8e5e278411f5ed2f1708d6010469a89c64de46c2
Christopher <crisson.christopher@gmail.com>
2020-06-23 11:34:18 -0400

    Updated tests

--- tests.py
+++ tests.py
@@ -3,6 +3,8 @@ import unittest
 from app import create_app, db
 from app.models import User, Book
 from config import Config
+from flask_mail import Mail
+from app.email import send_book_list_email, send_email
 
 class TestConfig(Config):
   TESTING = True
@@ -47,6 +49,57 @@ class UserModelCase(unittest.TestCase):
     db.session.commit()
     self.assertFalse(u.owns_book(b))
     self.assertEqual(u.owned.count(), 0)
+  
+  def test_send_email(self):
+    subject = 'test'
+    sender='test@testing.io'
+    recipients=['testing@testing.io']
+    text_body='text mctest'
+    html_body='<div>testing</div>'
+    mail = Mail(self.app)
+    with mail.record_messages() as outbox:
+      send_email(subject, sender, recipients, text_body, html_body)
+      assert len(outbox) == 1
+      assert outbox[0].subject == "test"
+      assert outbox[0].sender == sender
+      assert outbox[0].recipients == recipients
+      assert outbox[0].body == text_body
+
+  def test_send_book_list_email(self):
+    u = User(username='tim', email='tim@the.com')
+    b1title = 'hello'
+    b2title = 'world'
+    b1 = Book(title=b1title,
+      author='b',
+      date_of_purchase=datetime.utcnow(),
+      notes='Test Notes')
+    b2 = Book(title=b2title,
+      author='d',
+      date_of_purchase=datetime.utcnow(),
+      notes='Test Notes')
+    
+    db.session.add(u)
+    db.session.add(b1)
+    db.session.add(b2)
+    db.session.commit()
+    u.add_book_owned(b1)
+    u.add_book_owned(b2)
+    db.session.commit()
+    b = u.owned_books()
+    subject = 'test'
+    sender='test@testing.io'
+    recipients='testing@testing.io'
+    text_body='text mctest'
+    html_body='<div>testing</div>'
+    mail = Mail(self.app)
+    with mail.record_messages() as outbox:
+      send_book_list_email(u, recipients, b)
+      assert len(outbox) == 1
+      book1 = outbox[0].body.find('hello')
+      book2 = outbox[0].body.find('world')
+      assert book1 > 0
+      assert book2 > 0
+    
 
 if __name__ == '__main__':
   unittest.main(verbosity=2)
